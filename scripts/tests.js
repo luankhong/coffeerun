7eb4d7fc27cb42f7611434416b3b943da20fc6ff
CSUFLAPTOP-42\CampusUser <lkdrumandbass@csu.fullerton.edu>
2017-03-14 00:59:19 -0700

    uploaded file

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,46 +1,57 @@
-/*(function(window) {
-    'use strict';
-    var App = window.App || {};
-    window.App = App;
-})(window);*/
-
-QUnit.test('figure 8.10 code tests', function(assert) {
-    var App = window.App;
-    var ds = new App.DataStore();
-    ds.add('m@bond.com', 'tea');
-    ds.add('james@bond.com', 'eshpressho');
-    /*assert.equal(ds.getAll(), 'm@bond.com: "tea", james@bond.com: "eshpressho"');*/
-    assert.propEqual(ds.getAll(), {
-        'james@bond.com': 'eshpressho',
-        'm@bond.com': 'tea'
-    });
-    ds.remove('james@bond.com');
-    assert.propEqual(ds.getAll(), {
-        'm@bond.com': 'tea'
-    });
-    assert.equal(ds.get('m@bond.com'), 'tea');
-    assert.equal(ds.get('james@bond.com'), undefined);
+QUnit.test( 'hello test', function( assert ) {
+    assert.ok( 1 == '1', 'Passed!' );
 });
 
-QUnit.test('figure 8.32 code tests', function(assert) {
+QUnit.test('DataStore.prototype.get', function( assert ) {
     var App = window.App;
-    var myTruck = new App.Truck('ncc-1701', new App.DataStore());
-    myTruck.createOrder({
-        emailAddress: 'me@goldfinger.com',
-        coffee: 'double mocha'
-    });
-    myTruck.createOrder({
-        emailAddress: 'dr@no.com',
-        coffee: 'decaf'
-    });
-    myTruck.createOrder({
-        emailAddress: 'm@bond.com',
-        coffee: 'earl grey'
-    });
-    /*printOrders() doesnt actually return anything, so truck.js had to be changed
-    to return a string for the tests to work correctly*/
-    assert.equal(myTruck.printOrders(), 'Truck #ncc-1701 has pending orders:\nme@goldfinger.com: double mocha\ndr@no.com: decaf\nm@bond.com: earl grey\n');
-    myTruck.deliverOrder('dr@no.com');
-    myTruck.deliverOrder('m@bond.com');
-    assert.equal(myTruck.printOrders(), 'Truck #ncc-1701 has pending orders:\nme@goldfinger.com: double mocha\n');
+    var ds = new App.DataStore();
+    ds.add('me@goldfinger.com', 'double mocha');
+    ds.add('dr@no.com', 'decaf');
+    ds.add('m@bond.com', 'earl grey');
+
+    assert.ok(ds.get('m@bond.com') == 'earl grey', 'key of m@bond.com returned earl grey');
+    assert.ok(ds.get('dr@no.com') == 'decaf', 'key of dr@no.com returned decaf');
+    assert.ok(ds.get('me@goldfinger.com') == 'double mocha', 'key of me@goldfinger.com returned double mocha');
+});
+
+QUnit.test('DataStore.prototype.getAll', function( assert ) {
+    var App = window.App;
+    var ds = new App.DataStore();
+    ds.add('me@goldfinger.com', 'double mocha');
+    ds.add('dr@no.com', 'decaf');
+    ds.add('m@bond.com', 'earl grey');
+
+    var orders = ds.getAll();   //<--------------Testing getAll()
+    assert.ok(orders['m@bond.com'] == 'earl grey', 'key of m@bond.com returned earl grey');
+    assert.ok(orders['dr@no.com'] == 'decaf', 'key of dr@no.com returned decaf');
+    assert.ok(orders['me@goldfinger.com'] == 'double mocha', 'key of me@goldfinger.com returned double mocha');
+});
+
+//The problem I ran into is displaying diliverOrder() into Qunit assert..
+QUnit.test('Truck', function( assert ) {
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var truck = new Truck('ncc-1701', new DataStore());
+    truck.createOrder({emailAddress: 'me@goldfinger.com', coffee: 'double mocha'});
+    truck.createOrder({emailAddress: 'dr@no.com', coffee: 'decaf'});
+    truck.createOrder({emailAddress: 'm@bond.com', coffee: 'earl grey'});
+
+    var order1 = truck.db.get('me@goldfinger.com');
+    var order2 = truck.db.get('dr@no.com');
+    var order3 = truck.db.get('m@bond.com');
+    assert.ok(order1.emailAddress == 'me@goldfinger.com' && order1.coffee == 'double mocha', order1.emailAddress + ' ordered ' + order1.coffee);
+    assert.ok(order2.emailAddress == 'dr@no.com' && order2.coffee == 'decaf', order2.emailAddress + ' ordered ' + order2.coffee);
+    assert.ok(order3.emailAddress == 'm@bond.com' && order3.coffee == 'earl grey', order3.emailAddress + ' ordered ' + order3.coffee);
+
+    assert.equal(truck.summaryOrders(), 'Truck #ncc-1701 has pending orders: me@goldfinger.com double mocha | dr@no.com decaf | m@bond.com earl grey |');
+
+    truck.deliverOrder('dr@no.com');
+    assert.ok(!('dr@no.com' in truck), 'Delivering order for dr@no.com');
+
+    truck.deliverOrder('m@bond.com');
+    assert.ok(!('m@bond.com' in truck), 'Delivering order for m@bond.com');
+
+    assert.ok(truck.summaryOrders(), 'Truck #ncc-1701 has pending orders: me@goldfinger.com double mocha |');
+
 });
--- scripts/truck.js
+++ scripts/truck.js
@@ -8,35 +8,37 @@
     }
 
     Truck.prototype.createOrder = function(order) {
-        console.log('Adding order for ' + order.emailAddress);
+        //console.log('Adding order for ' + order.emailAddress);
         this.db.add(order.emailAddress, order);
     };
 
     Truck.prototype.deliverOrder = function(customerId) {
-        console.log('Delivering order for ' + customerId);
+        //console.log('Delivering order for ' + customerId);
         this.db.remove(customerId);
     };
 
     Truck.prototype.printOrders = function() {
         var customerIdArray = Object.keys(this.db.getAll());
-        var returnStr = '';
+
         console.log('Truck #' + this.truckId + ' has pending orders:');
-        returnStr += 'Truck #' + this.truckId + ' has pending orders:\n';
         customerIdArray.forEach(function(id) {
             console.log(this.db.get(id));
-            returnStr += this.db.get(id).emailAddress + ': ' + this.db.get(id).coffee + '\n';
         }.bind(this));
-        return returnStr;
+    };
+
+    Truck.prototype.summaryOrders = function() {
+        var customerIdArray = Object.keys(this.db.getAll());
+        var summary = '';
+
+        summary = 'Truck #' + this.truckId + ' has pending orders: ';
+        customerIdArray.forEach(function(id) {
+            summary = summary.concat(this.db.get(id).emailAddress + ' ' + this.db.get(id).coffee + ' | ');
+        }.bind(this));
+
+        return summary;
     };
 
     App.Truck = Truck;
     window.App = App;
 
 })(window);
-
-
-/*sample code
-var myTruck = new App.Truck ('007', new App.DataStore());
-myTruck.createOrder({ emailAddress: 'm@bond.com', coffee: 'earl grey'});
-myTruck.createOrder({ emailAddress: 'n@bond.com', coffee: 'earl green'});
-myTruck.createOrder({ emailAddress: 'o@bond.com', coffee: 'earl greg'});*/
